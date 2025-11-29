689a8c756b0be9ac76e3b2adc632d69f8a45405c
Sayfuddin Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2025-11-29 17:00:27 +0600

    feat: create a custom hook for upload image file to cloudinary

---
+++ src/hooks/image/useUploadImage.js
@@ -0,0 +1,9 @@
+import { useApiMutation } from "../useApiMutation";
+
+export const useUploadImage = () => {
+  return useApiMutation({
+    method: "POST",
+    url: "/uploads/image",
+    isPrivate: true,
+  });
+};
