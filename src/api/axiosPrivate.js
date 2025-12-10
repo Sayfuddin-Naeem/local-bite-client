61ca24cb721e4e0e631ce96e8c2174eac6058593
Sayfuddin Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2025-12-10 11:51:10 +0600

    refactor(axios): set up axios private for access token

--- src/api/axiosPrivate.js
+++ src/api/axiosPrivate.js
@@ -1,6 +1,22 @@
 import axios from "axios";
+import { getAuth } from "firebase/auth";
 
 export const axiosPrivate = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
 });
+
+// Attach Firebase ID Token to every request
+axiosPrivate.interceptors.request.use(
+  async (config) => {
+    const auth = getAuth();
+    const fbUser = auth.currentUser;
+
+    if (fbUser) {
+      const token = await fbUser.getIdToken();
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
